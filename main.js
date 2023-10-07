7fa1101d719f82af310b728f04c2ba79c01e8db0
KuzAnton8335 <ant2783@mail.com>
2023-10-07 20:36:57 +0300

    routing and creat element page

--- main.js
+++ main.js
@@ -1,30 +1,71 @@
+import Navigo from 'navigo';
 import 'normalize.css';
-import Swiper from 'swiper';
-import { Navigation, Thumbs } from 'swiper/modules';
+import { Footer } from './modules/Footer/footer';
+import { Header } from './modules/Header/header';
+import { Main } from './modules/Main/main';
 import './style.scss';
-// import Swiper styles
-import 'swiper/css';
 
-// swiper slider
-const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-new Swiper(".product__slider-main", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow-next",
-    prevEl: ".product__arrow-prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: swiperThumbnails,
-  },
-});
+const productSlider = () => {
+  Promise.all([
+    import("swiper/modules"),
+    import('swiper'),
+    import('swiper/css')
+  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+    const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    new Swiper.default(".product__slider-main", {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".product__arrow-next",
+        prevEl: ".product__arrow-prev",
+      },
+      modules: [Navigation, Thumbs],
+      thumbs: {
+        swiper: swiperThumbnails,
+      },
+    });
+  });
+}
 
 
+const init = () => {
+  new Header().mount();
+  new Main().mount();
+  new Footer().mount();
 
 
+  productSlider();
 
+  const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
+  router.on("/", () => {
+    console.log(' на главной');
+  })
+    .on('/category', (obj) => {
+      console.log(obj);
+      console.log('category');
+    })
+    .on('/favorite', () => {
+      console.log('favorite');
+    })
+    .on('/search', () => {
+      console.log('search');
+    })
+    .on('/product/:id', (obj) => {
+      console.log(obj);
+    })
+    .on('/cart', () => {
+      console.log('cart');
+    })
+    .on('/order', () => {
+      console.log('order');
+    })
+    .notFound(() => {
+      document.body.innerHTML = '<h2>Страница не найдена</h2>';
+    })
+  router.resolve();
+}
+init();
