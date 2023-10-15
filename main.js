3dd20a2a400d7b894b2869bcea7e3559cc0bfd5f
KuzAnton8335 <ant2783@mail.com>
2023-10-15 19:00:50 +0300

    productCard

--- main.js
+++ main.js
@@ -2,42 +2,20 @@ import Navigo from 'navigo';
 import 'normalize.css';
 import { BreadCrumbs } from './features/BreadCrumbs/breadCrumbs';
 import { Pagination } from './features/Pagination/Pagination';
+import { productSlider } from './features/productSlide/productSlide';
 import { Catalog } from './modules/Catalog/Catalog';
 import { Footer } from './modules/Footer/footer';
 import { Header } from './modules/Header/header';
 import { Main } from './modules/Main/main';
 import { Order } from './modules/Order/order';
+import { ProductCard } from './modules/ProductCard/productCard';
 import { Productlist } from './modules/ProductList/productlist';
 import { defaultPage } from './modules/defaultPage/defaultPage';
 import { Apiservice } from './services/Apiservice';
 import { FavoriteService } from './services/StorageServices';
 import './style.scss';
 
-const productSlider = () => {
-  Promise.all([
-    import("swiper/modules"),
-    import('swiper'),
-    import('swiper/css')
-  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
-    const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
-      spaceBetween: 10,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-    new Swiper.default(".product__slider-main", {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".product__arrow-next",
-        prevEl: ".product__arrow-prev",
-      },
-      modules: [Navigation, Thumbs],
-      thumbs: {
-        swiper: swiperThumbnails,
-      },
-    });
-  });
-}
+
 
 export const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
 
@@ -49,7 +27,7 @@ const init = () => {
   new Main().mount();
   new Footer().mount();
 
-  productSlider();
+
 
 
   router.on("/", async () => {
@@ -113,8 +91,28 @@ const init = () => {
     .on('/search', () => {
       console.log('search');
     })
-    .on('/product/:id', (obj) => {
-      console.log(obj);
+    .on('/product/:id', async (obj) => {
+      new Catalog().mount(new Main().element);
+      const data = await api.getProductById(obj.data.id);
+      console.log(data);
+      new BreadCrumbs().mount(new Main().element, [
+        {
+          text: data.category,
+          href: `/category?slug=${data.category}`,
+        },
+        {
+          text: data.name,
+        },
+      ]);
+      new ProductCard().mount(new Main().element, data);
+      productSlider();
+    }, {
+      leave(done) {
+        new Catalog().unmount();
+        new BreadCrumbs().unmount();
+        new ProductCard().unmount();
+        done();
+      },
     })
     .on('/cart', () => {
       console.log('cart');
